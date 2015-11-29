1c6fafcbd8697533c171b96b27f9c7fba5a4dbfe
carolight <caroline@archway.com.au>
2015-11-29 10:34:50 +1000

    Scribble Starter

---
+++ Scribble/CanvasView.swift
@@ -0,0 +1,52 @@
+//
+//  CanvasView.swift
+//  Scribble
+//
+//  Created by Caroline Begbie on 29/11/2015.
+//  Copyright © 2015 Caroline Begbie. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CanvasView: UIImageView {
+  
+  var drawingImage: UIImage?
+  
+  @IBInspectable var drawColor: UIColor = UIColor.redColor()
+  @IBInspectable var lineWidth: CGFloat = 10
+  
+  override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    guard let touch = touches.first else { return }
+    
+    UIGraphicsBeginImageContextWithOptions(bounds.size, false, 0.0)
+    let context = UIGraphicsGetCurrentContext()
+    
+    // Draw previous image into context
+    self.image?.drawInRect(bounds)
+    
+    let previousLocation = touch.previousLocationInView(self)
+    let location = touch.locationInView(self)
+    
+    // Set up the stroke
+    CGContextSetLineCap(context, .Round)
+    CGContextSetLineWidth(context, lineWidth)
+    drawColor.setStroke()
+
+    // Set up the points
+    CGContextMoveToPoint(context, previousLocation.x, previousLocation.y)
+    CGContextAddLineToPoint(context, location.x, location.y)
+    
+    // Draw the stroke
+    CGContextStrokePath(context)
+    
+    // Update image
+    self.image = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    
+  }
+  
+  func drawStroke(fromPoint: CGPoint, toPoint: CGPoint) {
+    
+  }
+}
