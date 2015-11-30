f7b9b7d49402fdfa96f1cd15f601d3a054ab2a40
carolight <caroline@archway.com.au>
2015-11-30 22:43:41 +1000

    Submitted Starter Sample

--- Scribble/CanvasView.swift
+++ Scribble/CanvasView.swift
@@ -1,28 +1,34 @@
-//
-//  CanvasView.swift
-//  Scribble
-//
-//  Created by Caroline Begbie on 29/11/2015.
-//  Copyright © 2015 Caroline Begbie. All rights reserved.
-//
+/*
+* Copyright (c) 2015 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
 
 import UIKit
 
 let π = CGFloat(M_PI)
 
-@IBDesignable
 class CanvasView: UIImageView {
   
-  var drawingImage: UIImage?
+  private var drawColor: UIColor = UIColor.redColor()
   
-  @IBInspectable var drawColor: UIColor = UIColor.redColor()
-  @IBInspectable var lineWidth: CGFloat = 6
-  
-  required init?(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
-    self.layer.borderColor = UIColor.blueColor().CGColor
-    self.layer.borderWidth = 1
-  }
+  private var lineWidth: CGFloat = 6
   
   override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
     guard let touch = touches.first else { return }
@@ -38,25 +44,36 @@ class CanvasView: UIImageView {
     // Update image
     self.image = UIGraphicsGetImageFromCurrentImageContext()
     UIGraphicsEndImageContext()
-    
   }
   
-  func drawStroke(context: CGContext?, touch: UITouch) {
+  private func drawStroke(context: CGContext?, touch: UITouch) {
     let previousLocation = touch.previousLocationInView(self)
     let location = touch.locationInView(self)
     
-    // Set up the default stroke
-    CGContextSetLineCap(context, .Round)
+    // set up line for drawing
+    lineWidth = lineWidthForDrawing(context, touch: touch)
     CGContextSetLineWidth(context, lineWidth)
     
+    // set color
     drawColor.setStroke()
     
     // Set up the points
     CGContextMoveToPoint(context, previousLocation.x, previousLocation.y)
     CGContextAddLineToPoint(context, location.x, location.y)
-    
     // Draw the stroke
     CGContextStrokePath(context)
-
+    
+  }
+  
+  private func lineWidthForDrawing(context: CGContext?, touch: UITouch) -> CGFloat {
+    
+    // Set up line for Drawing
+    CGContextSetLineCap(context, .Round)
+    
+    return lineWidth
+  }
+  
+  func clearCanvas() {
+    image = UIImage(named: "Background")
   }
 }
